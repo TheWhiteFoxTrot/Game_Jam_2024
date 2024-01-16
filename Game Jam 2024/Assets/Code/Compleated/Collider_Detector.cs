3e3c161cc3fff02d625b7916404007199d22162e
Ziondaman_SV <121035557+TheWhiteFoxTrot@users.noreply.github.com>
2024-01-16 05:48:43 +0100

    update v0.0.6

--- Game Jam 2024/Assets/Code/Compleated/Collider_Detector.cs
+++ Game Jam 2024/Assets/Code/Compleated/Collider_Detector.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 public class Collider_Detector : MonoBehaviour
 {
 
-    public Sprite Sprite_Switch_on_Trigger;
+    // Targeting 
+    // Adding more objects to trigger apone add a new gameobject name Target_For_Switch_[Letter]
+    // Adding a string with name Target_Name_[Letter of Target_For_Switch] 
+    // from there on void start conntect Target_Name_[Letter of Target] = Target_For_Switch_[Letter of Target].gameObject.name;
 
-    public GameObject Target_For_Switch;
-    string Target_Name;
+    public GameObject Target_For_Switch_A;
+    string Target_Name_A;
+
+
+    // Trigger 0 must be always true when starting the program.
+    // if more triggers are needed format is Trigger[Number].
+    bool Trigger0 = true;
+    bool Trigger1 = false;
 
     private void Start()
     {
         Debug.Log("Script started!");
-        Target_Name = Target_For_Switch.gameObject.name;
+
+        // Gethering the name of the object for trigger
+        Target_Name_A = Target_For_Switch_A.gameObject.name;
     }
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {
         Debug.Log("Debug: Collision = Active");
-        if (collision2D.gameObject.name == Target_Name)
+
+        if (Trigger0)
         {
-            Debug.Log("Debug: Target_For_Switch = Active");
+            if (collision2D.gameObject.name == Target_Name_A)
+            {
+                Debug.Log("Debug: Trigger 1 = Active");
+
+                // Asset switch 
+
+                // Activating next trigger
+                Trigger1 = true;
+                Trigger0 = false;
+            }
+            else if (Trigger1)
+            {
+                Debug.Log("Debug: Trigger 2 = Active");
+
+                // Asset switch 
+
+                // Activating next trigger
+                Trigger1 = false;
+            }
         }
     }
 }
