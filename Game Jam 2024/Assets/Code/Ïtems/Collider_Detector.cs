3bd2cd0f771a58d7355de01ebe90d46ea0214f56
Hampussss <hampus.skjonsbergbamegard@elev.ga.lbs.se>
2024-01-19 11:02:37 +0100

    Nigga place

--- Game Jam 2024/Assets/Code/Ïtems/Collider_Detector.cs
+++ Game Jam 2024/Assets/Code/Ïtems/Collider_Detector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collider_Detector : MonoBehaviour
 {
@@ -10,11 +11,18 @@ public class Collider_Detector : MonoBehaviour
     // Adding a string with name Target_Name_[Letter of Target_For_Switch] 
     // from there on void start conntect Target_Name_[Letter of Target] = Target_For_Switch_[Letter of Target].gameObject.name;
 
-    public GameObject objectToSpawn
+    public bool SummonObject;
+    public GameObject objectToSpawn;
+
+    public bool lither;
+    private GameObject Prefab;
+
     public GameObject Target_For_Switch_A;
     string Target_Name_A;
     public Sprite StartAsset;
     public Sprite Transformed;
+
+    public bool NectLevel;
 
     // Trigger 0 must be always true when starting the program.
     // if more triggers are needed format is Trigger[Number].
@@ -28,6 +36,12 @@ public class Collider_Detector : MonoBehaviour
         this.gameObject.GetComponent<SpriteRenderer>().sprite = StartAsset;
         // Gethering the name of the object for trigger
         Target_Name_A = Target_For_Switch_A.gameObject.name;
+
+    }
+
+    private void Update()
+    {
+       
     }
 
     void OnCollisionEnter2D(Collision2D collision2D)
@@ -36,14 +50,28 @@ public class Collider_Detector : MonoBehaviour
 
         if (Trigger0)
         {
+
             if (collision2D.gameObject.name == Target_Name_A)
             {
                 Debug.Log("Debug: Trigger 1 = Active");
-                Instantiate(objectToSpawn);
+                if (SummonObject)
+                {
+                    if (lither)
+                    {
+                        Instantiate(objectToSpawn);
+                    }
+                    Instantiate(objectToSpawn);
+                }
                 this.gameObject.GetComponent<SpriteRenderer>().sprite = Transformed;
                 // Activating next trigger
                 Trigger1 = true;
                 Trigger0 = false;
+
+                if (NectLevel)
+                {
+                    Debug.Log("Next");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
             }
             else if (Trigger1)
             {
